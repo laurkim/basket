3ca75866cfd6fad896d8673d6953d16609ea1799
laurkim <laurakeem@gmail.com>
2018-11-19 05:29:16 -0500

    Construct skeleton of app

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.scss"; 
+
+const App = () => {
+    return (
+        <div>
+            <h1 id="app">Basket Rendered</h1>
+        </div>
+    )
+}
+
+export default App;
