ab5c7855deafa4e259db837f11ecc59a33e83794
laurkim <laurakeem@gmail.com>
2018-11-19 14:59:20 -0500

    Add functionality to add item to cart

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import ReactModal from "react-modal";
 import "../App.scss";
+ReactModal.setAppElement('#base');
 
-const ItemDetail = props => {
-    console.log("props are", props)
-    return (
-        <h3>{props.item}</h3>
-    )
+class ItemDetail extends Component {
+    state = {
+        displayModal: false
+    }
+
+    displayBuyingModal = () => {
+        this.setState({ 
+            displayModal: true
+        })
+    }
+
+    addItemToCart = () => {
+        this.props.addItem(this.props.item)
+        this.setState({
+            displayModal: false
+        })
+    }
+
+    render() {
+        return (
+            <div id="item-detail">
+                <h3>{this.props.item}</h3>
+                <button onClick={this.displayBuyingModal}>Pick Quantity</button>
+                <ReactModal
+                    isOpen={this.state.displayModal}
+                    contentLabel="Add Item">
+                    <button onClick={this.addItemToCart}>Add to Cart</button>
+                </ReactModal>
+            </div>
+        )
+    }
 }
 
 export default ItemDetail;
+
+
+
+
